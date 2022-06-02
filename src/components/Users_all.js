19f2b7b50eecf54b441b5a8141b2d759e7c4d15b
YonatanSchuster <yonatanmsch@gmail.com>
2022-06-02 20:57:57 +0300

    now we can see the data from the db in the web consol

--- src/components/Users_all.js
+++ src/components/Users_all.js
@@ -16,7 +16,7 @@ function Users_all() {
   return (
     <>
       <div className="flex p-3 justify-center flex-col">
-        <p>this is users management ppage</p>
+        <p>this is users management page</p>
       </div>
     </>
   );
